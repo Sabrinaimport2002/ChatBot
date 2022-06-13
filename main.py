9506b7f0bda6522a195cb38299121d2eabbeeeab
Felipe <lipe.gigante2004@gmail.com>
2022-06-13 01:42:11 -0300

    Alteração de Interface

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 #import kivy
-from tkinter import Label
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
 
 
-class HomeScreen(BoxLayout):
+class HomeScreen(ScrollView):
     def __init__ (self, tarefas, **kwargs):
         super().__init__(**kwargs)
         for tarefa in tarefas:
-            self.add_widget(Label(text=tarefa))
+            self.ids.box.add_widget(Message(text=tarefa))
+
+class Message(BoxLayout):
+    def __init__(self, text='', **kwargs):
+        super().__init__(**kwargs)
+        self.ids.label.text = text
 
 class Interface(App):
     def build(self):
-        return HomeScreen(["ChatBot"])
+        return HomeScreen(["ChatBot", "ChatBot", "ChatBot", "ChatBot"])
     
 Interface().run()
